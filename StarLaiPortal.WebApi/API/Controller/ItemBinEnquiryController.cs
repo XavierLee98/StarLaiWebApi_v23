b61d4a174abb40304905aa1a54d4f298861b77a6
leekx430 <leekx430@yahoo.com>
2025-08-27 09:19:45 +0800

    Add Item Bin Enquiry Module

---
+++ StarLaiPortal.WebApi/API/Controller/ItemBinEnquiryController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using DevExpress.ExpressApp.Core;
+using DevExpress.ExpressApp.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+
+namespace StarLaiPortal.WebApi.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ItemBinEnquiryController : ControllerBase
+    {
+        private IConfiguration Configuration { get; }
+        IObjectSpaceFactory objectSpaceFactory;
+        ISecurityProvider securityProvider;
+        public ItemBinEnquiryController(IConfiguration configuration, IObjectSpaceFactory objectSpaceFactory, ISecurityProvider securityProvider)
+        {
+            this.objectSpaceFactory = objectSpaceFactory;
+            this.securityProvider = securityProvider;
+            this.Configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string whscode, string bincode, string itemcode)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
+                {
+                    string json = "";
+                    json = JsonConvert.SerializeObject(new { whscode, bincode, itemcode });
+                    var val = conn.Query($"exec sp_GetInquiry 'ItemBinEnquiry', '{json}'").ToList();
+
+                    return Ok(JsonConvert.SerializeObject(val, Formatting.Indented));
+                }
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
