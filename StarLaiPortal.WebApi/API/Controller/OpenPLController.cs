39b0c69d4014d77922d120eccf1661f102d31a74
leekx430 <leekx430@yahoo.com>
2026-03-12 10:38:51 +0800

    refactor PL and Loading, block create and update when fail to generate Load and DO

--- StarLaiPortal.WebApi/API/Controller/OpenPLController.cs
+++ StarLaiPortal.WebApi/API/Controller/OpenPLController.cs
@@ -7,22 +7,17 @@ using System.Data.SqlClient;
 using Dapper;
 using StarLaiPortal.WebApi.Model;
 using DevExpress.ExpressApp.Security;
-using StarLaiPortal.Module.BusinessObjects.View;
 using StarLaiPortal.Module.BusinessObjects;
-using StarLaiPortal.Module.BusinessObjects.Sales_Order;
 using DevExpress.Data.Filtering;
-using Newtonsoft.Json.Linq;
 using StarLaiPortal.Module.BusinessObjects.Pick_List;
 using StarLaiPortal.WebApi.Helper;
 using System.Dynamic;
 using DevExpress.Xpo;
-using System.Text.Json;
 using StarLaiPortal.Module.Controllers;
 using StarLaiPortal.Module.BusinessObjects.Pack_List;
 using StarLaiPortal.Module.BusinessObjects.Load;
 using StarLaiPortal.Module.BusinessObjects.Delivery_Order;
 using StarLaiPortal.Module.BusinessObjects.Setup;
-using System.Diagnostics;
 
 namespace StarLaiPortal.WebApi.API.Controller
 {
@@ -110,8 +105,6 @@ namespace StarLaiPortal.WebApi.API.Controller
 
                     jsonString = jsonString = jsonString.Replace("'", "''");
 
-                    var test = $"{obj}";
-
                     var validatejson = conn.Query<ValidateJson>($"exec ValidateJsonInput 'PickListDetailsActual', '{jsonString}'").FirstOrDefault();
                     if (validatejson.Error)
                     {
@@ -120,33 +113,6 @@ namespace StarLaiPortal.WebApi.API.Controller
                 }
 
                 var detailsObject = (IEnumerable<dynamic>)dynamicObj.PickListDetailsActuals;
-
-                //if (detailsObject != null)
-                //{
-                //    foreach (var line in detailsObject)
-                //    {
-                //        if (string.IsNullOrEmpty(line.ToBin))
-                //        {
-                //            return Problem("The ToBin value is null. Please select Tobin.");
-                //        }
-                //    }
-
-                //    using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
-                //    {
-                //        var itembins = detailsObject.GroupBy(x => new { x.ItemCode, x.FromBin }).Select(y => new { y.Key.ItemCode, y.Key.FromBin, PickQty = y.Sum(x => x.PickQty) });
-
-                //        foreach (var line in itembins)
-                //        {
-                //            string json = JsonConvert.SerializeObject(new { itemcode = line.ItemCode, bincode = line.FromBin, quantity = line.PickQty });
-
-                //            var validateBalance = conn.Query<ValidateJson>($"exec sp_beforedatasave 'ValidateStockBalance', '{json}'").FirstOrDefault();
-                //            if (validateBalance.Error)
-                //            {
-                //                return Problem(validateBalance.ErrorMessage);
-                //            }
-                //        }
-                //    }
-                //}
 
                 if (detailsObject != null)
                 {
@@ -187,28 +153,21 @@ namespace StarLaiPortal.WebApi.API.Controller
                 var userId = security.UserId;
                 var userName = security.UserName;
 
-
                 if (plobj.Status != DocStatus.Draft)
                 {
                     return Problem($"Update Failed. Pick List No. {plobj.DocNum} already {plobj.Status}.");
                 }
 
-                using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
+                foreach (var line in plobj.PickListDetails)
                 {
-                    foreach (var line in plobj.PickListDetails)
-                    {
-                        string itemjson = JsonConvert.SerializeObject(new { picklist = dynamicObj.PickOid, picklistdetailoid = line.Oid });
-                        var pickedQty = conn.Query<decimal>($"exec sp_afterdatasave 'PickListDetailsActual', '{itemjson}'").FirstOrDefault();
+                    var actualPickQty = plobj.PickListDetailsActual.Where(x => x.PickList == line.PickList && x.PickListDetailOid == line.Oid).Sum(y => y.PickQty);
 
-                        var inputQty = detailsObject.Where(x => x.PickList == dynamicObj.PickOid && x.PickListDetailOid == line.Oid).Sum(y => (decimal)y.PickQty);
+                    var inputQty = detailsObject.Where(x => x.PickList == dynamicObj.PickOid && x.PickListDetailOid == line.Oid).Sum(y => (decimal)y.PickQty);
 
-                        if (pickedQty + inputQty > line.PlanQty) return Problem($"Actual pick quantity ({(pickedQty + inputQty):F2}) cannot more than plan quantity ({line.PlanQty:F2}). Please check picklist in portal.");
-                    }
+                    if (actualPickQty + inputQty > line.PlanQty) return Problem($"Actual pick quantity cannot more than plan quantity. Please check picklist in portal.");
                 }
 
                 plobj.Picker = plOS.GetObjectByKey<ApplicationUser>(userId);
-
-                plOS.CommitChanges();
 
                 LogHelper.CreateLog(Configuration.GetConnectionString("ConnectionString"), userId.ToString(), "Picking", obj);
 
@@ -236,19 +195,17 @@ namespace StarLaiPortal.WebApi.API.Controller
                     objs.Add(curobj);
                 }
 
-                newObjectSpace.CommitChanges();
-
-                //Update Quantity
                 using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
                 {
                     foreach (var line in plobj.PickListDetails)
                     {
-                        string json = JsonConvert.SerializeObject(new { picklist = dynamicObj.PickOid, picklistdetailoid = line.Oid });
-                        var pickqty = conn.Query<decimal>($"exec sp_afterdatasave 'PickListDetailsActual', '{json}'").FirstOrDefault();
+                        var actualPickQty = plobj.PickListDetailsActual.Where(x => x.PickList == line.PickList && x.PickListDetailOid == line.Oid).Sum(y => y.PickQty);
 
-                        if (pickqty > line.PlanQty) return Problem($"Actual pick quantity cannot more than plan quantity. Please check picklist in portal.");
+                        var inputQty = detailsObject.Where(x => x.PickList == dynamicObj.PickOid && x.PickListDetailOid == line.Oid).Sum(y => (decimal)y.PickQty);
 
-                        line.PickQty = pickqty;
+                        if (actualPickQty + inputQty > line.PlanQty) return Problem($"Actual pick quantity cannot more than plan quantity. Please check picklist in portal.");
+
+                        line.PickQty = actualPickQty + inputQty;
 
                         if (dynamicObj.PickListDetails != null && dynamicObj.PickListDetails.Count > 0)
                         {
@@ -266,13 +223,7 @@ namespace StarLaiPortal.WebApi.API.Controller
 
                 plobj.Status = DocStatus.Submitted;
 
-                plOS.CommitChanges();
-
-                using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
-                {
-                    string json = JsonConvert.SerializeObject(new { oid = objs.FirstOrDefault().PickList.Oid, username = userName });
-                    conn.Query($"exec sp_afterdatasave 'PickListStatus', '{json}'");
-                }
+                plobj.Save();
 
                 IObjectSpace packos = objectSpaceFactory.CreateObjectSpace<PackList>();
                 IObjectSpace loados = objectSpaceFactory.CreateObjectSpace<Load>();
@@ -282,8 +233,16 @@ namespace StarLaiPortal.WebApi.API.Controller
 
                 GeneralControllers con = new GeneralControllers();
                 var result = con.GenerateAutoDO(Configuration.GetConnectionString("ConnectionString"), objs.FirstOrDefault().PickList, newObjectSpace, packos, loados, deliveryos, companyPrefix);
+                if (result == 0) throw new Exception($"Fail to generate Auto Delivery for Pick List ({plobj.DocNum}). Please retry again.");
 
-                if (result == 0) throw new Exception($"Fail to generate Auto Delivery for Pick List ({plobj.DocNum}). ");
+                plOS.CommitChanges();
+                newObjectSpace.CommitChanges();
+
+                using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
+                {
+                    string json = JsonConvert.SerializeObject(new { oid = objs.FirstOrDefault().PickList.Oid, username = userName });
+                    conn.Query($"exec sp_afterdatasave 'PickListStatus', '{json}'");
+                }
 
                 return Ok(new { oid = plobj.Oid, docnum = plobj.DocNum, IsAutoDO = result });
 
@@ -293,22 +252,5 @@ namespace StarLaiPortal.WebApi.API.Controller
                 return Problem(ex.Message);
             }
         }
-
     }
 }
-
-//foreach (var aaa in objs)
-//{
-//    using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
-//    {
-//        string json = JsonConvert.SerializeObject(new { oid = aaa.Oid });
-//        conn.Query($"exec sp_afterdatasave 'PickListDetailsActual', '{json}'");
-//    }
-//}
-
-//using (SqlConnection conn = new SqlConnection(Configuration.GetConnectionString("ConnectionString")))
-//{
-//    string json = JsonConvert.SerializeObject(dynamicObj.PickListDetails);
-//    conn.Query($"exec sp_updateData 'SetPickListDetailsReason', '{json}'");
-//}
-
